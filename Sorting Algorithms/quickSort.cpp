10db9d5ba0f69834bbd353a9df88d95131e429c9
Aman Aswal <amanpotato11@gmail.com>
2020-07-25 17:25:11 +0530

    Update quickSort.cpp

--- Sorting Algorithms/quickSort.cpp
+++ Sorting Algorithms/quickSort.cpp
@@ -16,19 +16,19 @@ int Partition(int *arr, int start, int end){
     return partitionIndex;
 }
 
-void mergeSort(int *arr, int start, int end){
+void quickSort(int *arr, int start, int end){
     if(start < end)
     {
         int partitionIndex = Partition(arr, start, end); // calling partition
-        mergeSort(arr, 0, partitionIndex-1);
-        mergeSort(arr, partitionIndex+1, end);
+        quickSort(arr, 0, partitionIndex-1);
+        quickSort(arr, partitionIndex+1, end);
     }
 }
 
 int main()
 {
     int arr[] = {7,6,5,4,3,2,1,0};
-    mergeSort(arr, 0, 7);
+    quickSort(arr, 0, 7);
 
     for(int i=0; i<8; i++){
         cout<<arr[i]<<" ";

2c8294cd54e6bde85f5b7ab3fdd8403c33d8c8dc
Aman Aswal <amanpotato11@gmail.com>
2020-08-10 19:51:51 +0530

    Create Ceil of an element.cpp

---
+++ binarySearch/Ceil of an element.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int search(int *arr, int n, int x)
+{
+    int result=-1;
+    int start = 0, end = n-1;
+    while (start <= end)
+    {
+        int mid = start + (end-start)/2;
+
+        if (x == arr[mid])
+            return arr[mid];
+
+        else if (x < arr[mid]) {
+            result = arr[mid];
+            end = mid-1;
+        }
+        else {
+
+            start = mid+1;
+        }
+    }
+    return result;
+
+}
+
+int main()
+{
+    int arr[] ={ 1, 2, 3, 4, 8, 9, 10 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x;
+    cout<<"Enter a number whose floor value you want: ";
+    cin>>x;
+
+    int index = search(arr, n, x);
+    cout<<"Floor element is: "<<index<<endl;
+    return 0;
+}
